82aefdcfc5dee85ad218d5aebbae174c1c9355c3
Tizian Breuch <Tizian.Breuch@edvring.de>
2023-04-18 15:57:18 +0200

    abschluss

--- app.js
+++ app.js
@@ -1,67 +1,68 @@
 const express = require("express");
 const { expressjwt: jwt } = require("express-jwt");
-const jwksRsa = require('jwks-rsa');
+const jwksRsa = require("jwks-rsa");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const jsonwebtoken = require("jsonwebtoken");
 const fs = require("fs");
-var cors = require('cors')
+var cors = require("cors");
+const { verify } = require("crypto");
+const { log } = require("console");
+// const router = express.Router();
 
 const PORT = 3001;
-
 const app = express();
-app.use(cors())
+app.use(cors());
 
+require("crypto").randomBytes(64).toString("hex");
 app.use(bodyParser.json());
 
-app.route("/api/login").post(loginRoute());
 
-const RSA_PRIVATE_KEY = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCQgX+jNSW05HAk/vIiKmKfB8zl
-L9afFPFtScnAZcIjPnjuFUGJbwerFcFrhDccdYpInc8C574Pt9UYpbZSI5YY2il8
-Ce2/0XlJR7hZfd/a2yFiW7+Uo/oK1LR+ghiKcdMu5OeKg7SaVFVO+xdcNqMS6igU
-q094Ab4GevXrdInEpQIDAQAB`; //fs.readFileSync('./demos/private.key');
+const TOKEN_SECRET = "qwdqwdqwdqdw"; //fs.readFileSync("./key.pub", "utf8");    bzw secret
 
-function validateEmailAndPassword(){
-    return true
+//////////
+//////////
+
+app.post("/api/createNewUser", async (req, res) => {
+  const token = await generateAccessToken({ username: req.body.username });
+  res.json(token);
+  console.log(token)
+});
+
+async function generateAccessToken(username) {
+  return jsonwebtoken.sign(username, TOKEN_SECRET, { expiresIn: "100s" });
 }
-function findUserIdForEmail(email){
-   if(email = "breuch.tizian@gmail.com"){
-    return 1
-   };
+
+///////
+///////
+
+// app.get("/api/auth", authenticateToken, (req, res) => {
+//   res.json("token valid");
+// });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jsonwebtoken.verify(token, TOKEN_SECRET, (err, user) => {
+    console.log(user);
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
     
+    next();
+  });
 }
 
-function loginRoute(req, res) {
-  const email = req.body.email;
-   const password = req.body.password;
-  if (validateEmailAndPassword()) {
-    const userId = findUserIdForEmail(email,password);
 
-    const jwtBearerToken = jsonwebtoken.sign({}, RSA_PRIVATE_KEY, {
-      algorithm: "RS256",
-      expiresIn: 120,
-      subject: userId,
-    });
-    res.cookie("SESSIONID", jwtBearerToken, { secure: true }); //httpOnly: true,secure: true
-    // send the JWT back to the user
-    // TODO - multiple options available
-  } else {
-    // send status 401 Unauthorized
-    res.sendStatus(401);
-  }
-}
-const checkIfAuthenticated = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksUri: "https://angularuniv-security-course.auth0.com/.well-known/jwks.json"
-    }),
-    algorithms: ['RS256']
-}); 
 
-app.route('/api/lessons')
-    .get(checkIfAuthenticated);
 
+const authRoute = require('./routes/auth')
+app.use("/api/auth", authenticateToken, authRoute, (req, res) => {
+  res.json("token valid");
+});
 
 app.listen(PORT, function (err) {
   if (err) console.log("Error in server setup");
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/1", async (req, res) => {
+    
+      res.json("route: 1");
+    
+  });
+
+
+
+module.exports = router;
